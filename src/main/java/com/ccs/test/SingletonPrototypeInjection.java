11090b15d0448f683e67ec24408337627fa8473c
prudhvikrishna.k <prudhvikrishna.k@gmail.com>
2015-05-04 19:05:06 -0500

    FileUploadSpring
    Uploading a file to database

---
+++ src/main/java/com/ccs/test/SingletonPrototypeInjection.java
@@ -0,0 +1,33 @@
+package com.ccs.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class SingletonPrototypeInjection {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ApplicationContext context = 
+		    	 new ClassPathXmlApplicationContext(new String[] {"singletonPrototypeInjection.xml"});
+		 
+		StudentSingleton custA = (StudentSingleton)context.getBean("student");
+		    	custA.setMessage("Message by custA");
+		    	
+		    	custA.getAddress().setMessage("This is address");
+		    	
+		    	System.out.println("Message : " + custA.getMessage());
+		 	    System.out.println(custA.getAddress().getMessage());
+		    	
+		    	//retrieve it again
+		    	StudentSingleton custB = (StudentSingleton)context.getBean("student");
+		    	custB.getAddress().setMessage("This is address 222");
+		    	System.out.println("Message : " + custB.getMessage());
+		    	System.out.println(custB.getAddress().getMessage());
+		    	System.out.println(custA.getAddress().getMessage());
+	}
+
+}
